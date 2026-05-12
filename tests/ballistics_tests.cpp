0969a538d45e49a1ea93ff8b34b8a5285bb771ff
Volodymyr Semenyakin <volodymyr.semenyakin@fracturedbyte.com>
2026-05-12 21:54:20 +0300

    - Added test sources - Added CMake test configuration and CTest setup - Added test presets - ADded test data

---
+++ tests/ballistics_tests.cpp
@@ -0,0 +1,36 @@
+﻿#include <gtest/gtest.h>
+#include "ballistics.hpp"
+
+TEST(Ballistics, Test1) {
+
+    const float attackSpeed = 10.f;
+    const float zd = 100.f;
+    const float ammo_m = 0.35f;
+    const float ammo_d = 0.07f;
+    const float ammo_l = 0.f;
+
+    float ammo_fligthTime;
+    float h;
+	computeAmmoDrop(ammo_fligthTime, h, attackSpeed, zd, ammo_m, ammo_d, ammo_l);
+
+    EXPECT_NEAR(ammo_fligthTime, 5.74976f, 0.0001f);
+    EXPECT_NEAR(h, 37.1102f, 0.0001f);
+}
+
+TEST(Ballistics, Test2) {
+
+    // const float attackSpeed = 10.f;
+    // const float zd = 100.f;
+    // const float ammo_m = 0.35f;
+    // const float ammo_d = 0.07f;
+    // const float ammo_l = 0.f;
+
+    // float ammo_fligthTime;
+    // float h;
+	// computeAmmoDrop(ammo_fligthTime, h, attackSpeed, zd, ammo_m, ammo_d, ammo_l);
+
+    // EXPECT_NEAR(ammo_fligthTime, 5.74976f, 0.0001f);
+    // EXPECT_NEAR(h, 37.1102f, 0.0001f);
+}
+
+
