7084ab592ae41bad3ef6e33beedcde8ba542b8e1
Volodymyr Semenyakin <volodymyr.semenyakin@fracturedbyte.com>
2026-05-13 16:26:43 +0300

    - Put additional print logic into DebugPrint only mode

--- src/main.cpp
+++ src/main.cpp
@@ -99,6 +99,7 @@ int main() {
 		return 1;
 	}
 
+#ifdef DebugPrint
 
 	std::cout << "==========================={{{" << std::endl;
 
@@ -108,14 +109,20 @@ int main() {
 	std::cout << "ammo_d: " << ammo_d << std::endl;
 	std::cout << "ammo_l: " << ammo_l << std::endl;
 
+#endif //DebugPrint
+
 	float ammo_fligthTime;
 	float h;
 	computeAmmoDrop(ammo_fligthTime, h, attackSpeed, zd, ammo_m, ammo_d, ammo_l);
+
+#ifdef DebugPrint
 
 	std::cout << "ammo_fligthTime: " << ammo_fligthTime << std::endl;
 	std::cout << "h: " << h << std::endl;
 
 	std::cout << "===========================}}}" << std::endl;
+
+#endif //DebugPrint
 
 	//--- Drop point ---
 
